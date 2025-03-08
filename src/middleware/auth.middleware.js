a3a7eb0501204e3dc8ce33fb87ba6cafddc7a2d2
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-03-09 04:06:55 +0600

    some changes

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,30 +6,24 @@ const verifyJwt = async (req, res, next) => {
   try {
     let token = req.cookies?.accessToken || req.headers.authorization;
 
-    // টোকেন না থাকলে Unauthorized error
     if (!token) {
       throw new ApiError(401, "Unauthorized: No token provided.");
     }
 
-    // যদি হেডারে টোকেন থাকে, তবে "Bearer " অংশটি সরিয়ে ফেলতে হবে
     if (req.headers.authorization?.startsWith("Bearer ")) {
       token = req.headers.authorization.split(" ")[1];
     }
 
-    // টোকেন যাচাই
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
 
-    // ইউজার খোঁজা, তবে পাসওয়ার্ড আর রিফ্রেশ টোকেন ছাড়া
     const user = await User.findById(decoded?._id).select(
       "-password -refreshToken"
     );
 
-    // যদি ইউজার না মেলে, তাহলে Unauthorized error
     if (!user) {
       throw new ApiError(401, "Unauthorized: Invalid token.");
     }
 
-    // ইউজার অবজেক্ট রিকোয়েস্টে সংযুক্ত করা
     req.user = user;
 
     next();
